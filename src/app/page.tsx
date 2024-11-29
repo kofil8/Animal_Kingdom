94e22925d31b7293703d0d0e90b7ff868768dfc1
kofil8 <mohammedkofil8@gmail.com>
2024-11-29 23:15:03 +0600

    Finally done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import HomePage from './Commponents/HomePage/Home';
 configDotenv();
 
 export default function Home() {
-  return (
-    <main>
-      <HomePage />
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <HomePage />
+        </main>
+    );
 }
